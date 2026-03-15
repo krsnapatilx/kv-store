eb59d987056b00e668c3187f6b610e83aa7cb484
krsnapatilx <krsnapatilx@gmail.com>
2026-03-15 05:53:25 -0700

    fix: correct cargo fmt lint command in kv-store benchmark

--- benches/kvstore_bench.rs
+++ benches/kvstore_bench.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
 use kv_store::KvStore;
 use std::fs::{create_dir_all, remove_dir_all};
 use std::hint::black_box;
